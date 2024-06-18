9d1d2c0f1e27ac407be57ccd4a7be36da68c8dd7
yash <yashgouravkar@gmail.com>
2024-06-18 10:30:36 +0530

    plus 4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,43 @@
 import Footer from "@/components/footer";
 import Hero from "@/components/hero";
 import Nav from "@/components/landing-nav";
+import IconCloud from "@/components/ui/icon-cloud";
 
 export default function Home() {
   return (
     <main className="relative flex min-h-screen flex-col items-center justify-center gap-4 bg-gradient-to-br from-[#0f0f19] to-[#0c0c14] md:from-[#000] md:via-[#181828] md:to-[#000]">
       <Nav />
       <Hero />
+      <IconCloudDemo />
       <Footer />
     </main>
+  );
+}
+
+const slugs = [
+  "typescript",
+  "react",
+  "nodedotjs",
+  "express",
+  "mongodb",
+  "tailwindcss",
+  "nextdotjs",
+  "mysql",
+  "postgres",
+  "redis",
+  "trpc",
+  "docker",
+  "vercel",
+  "prisma",
+  "postgresql",
+  "vercel",
+  "docker",
+];
+
+export function IconCloudDemo() {
+  return (
+    <div className="relative flex h-full w-full max-w-[43rem] items-center justify-center overflow-hidden rounded-lg bg-background px-20 pb-20 pt-8 brightness-75">
+      <IconCloud iconSlugs={slugs} />
+    </div>
   );
 }
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -6,12 +6,14 @@ import { Particles } from "./ui/particles";
 
 const Hero = () => {
   return (
-    <div className="flex w-4/5 flex-col items-center justify-center gap-6 ">
+    <div className="flex w-4/5 flex-col items-center justify-center gap-6">
       <Particles
-        className="animate-fade-in absolute inset-0 "
+        className="animate-fade-in absolute inset-0"
         color="#8daab5"
         quantity={60}
         ease={30}
+        vx={0.1}
+        vy={-0.3}
       />
       <ul
         className="text- flex animate-fade-down gap-4 text-xs text-gray-400 md:text-sm"
@@ -27,14 +29,14 @@ const Hero = () => {
           Projects
         </Link>
         <Link
-          className=" z-10 duration-500 hover:font-semibold"
+          className="z-10 duration-500 hover:font-semibold"
           href={"/contact"}
         >
           Links
         </Link>
       </ul>
       <h1
-        className="animate-fade-down cursor-default bg-gradient-to-r from-slate-600 via-slate-200 to-slate-600 bg-clip-text text-4xl font-semibold tracking-tight text-transparent md:text-5xl lg:text-6xl lg:tracking-[-0.24rem] "
+        className="animate-fade-down cursor-default bg-gradient-to-r from-slate-600 via-slate-200 to-slate-600 bg-clip-text text-4xl font-semibold tracking-tight text-transparent md:text-5xl lg:text-6xl lg:tracking-[-0.24rem]"
         style={{
           animationDelay: "0.20s",
           animationFillMode: "forwards",
@@ -43,15 +45,15 @@ const Hero = () => {
         <Balancer>{self.name}</Balancer>
       </h1>
       <h5
-        className="md:text-md animate-fade-down text-center text-sm text-gray-400 "
+        className="md:text-md animate-fade-down text-center text-sm text-gray-400"
         style={{
           animationDelay: "0.3s",
           animationFillMode: "forwards",
         }}
       >
         <Balancer>
-          Hi, I&apos;m a software developer building full-stack applications
-          during the day and looking at open-source projects at night.
+          Full-time software developer building applications with React and
+          TRPC.
         </Balancer>
       </h5>
       <Link
