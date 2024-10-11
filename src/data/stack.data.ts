86118e87f95336ef2804ff783dc46fb9094ba598
yash <yashgouravkar@gmail.com>
2024-10-11 07:55:54 +0530

    stacks

--- src/data/stack.data.ts
+++ src/data/stack.data.ts
@@ -4,7 +4,6 @@ export const stackl = [
   "CSS",
   "Docker",
   "Express",
-  "ESLint",
   "HTML",
   "JavaScript",
   "MongoDB",
@@ -14,12 +13,9 @@ export const stackl = [
   "PostgreSQL",
   "Prisma",
   "React",
-  "Redis",
-  "SQLite",
   "TailwindCSS",
   "TRPC",
   "TypeScript",
-  "Vite",
 ] as const;
 
 export const stacks = stackl.map((label) => ({
@@ -36,23 +32,19 @@ export const stacks = stackl.map((label) => ({
               ? "Nodedotjs"
               : label === "PostgreSQL"
                 ? "Postgresql"
-                : label === "SQLite"
-                  ? "Sqlite"
-                  : label === "TypeScript"
-                    ? "Typescript"
-                    : label === "NextJS"
-                      ? "Nextdotjs"
-                      : label === "TailwindCSS"
-                        ? "Tailwindcss"
-                        : label === "TRPC"
-                          ? "Trpc"
-                          : label === "MongoDB"
-                            ? "Mongodb"
-                            : label === "MySQL"
-                              ? "Mysql"
-                              : label === "ESLint"
-                                ? "Eslint"
-                                : label
+                : label === "TypeScript"
+                  ? "Typescript"
+                  : label === "NextJS"
+                    ? "Nextdotjs"
+                    : label === "TailwindCSS"
+                      ? "Tailwindcss"
+                      : label === "TRPC"
+                        ? "Trpc"
+                        : label === "MongoDB"
+                          ? "Mongodb"
+                          : label === "MySQL"
+                            ? "Mysql"
+                            : label
     }`
   ],
 }));
