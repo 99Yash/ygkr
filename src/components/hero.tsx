529c22192af502b4e024225dbe2ac1423639f45a
yash <yashgouravkar@gmail.com>
2025-03-24 08:20:35 +0530

    about

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -47,24 +47,27 @@ export default function Hero() {
             duration: 0.5,
           }}
         >
-          <h1 className="bg-gradient-to-b from-slate-300/80 via-slate-300/85 to-slate-500 bg-clip-text font-title text-2xl font-bold leading-9 text-transparent sm:text-4xl sm:leading-[3.5rem]">
+          <h1 className="bg-gradient-to-b from-slate-300/80 via-slate-300/85 to-slate-500 bg-clip-text font-title text-2xl font-bold leading-9 text-transparent sm:text-4xl sm:leading-[2.75rem]">
             Beautiful frontends. Secure backends. <br />
           </h1>
           <p
             className={cn(
-              "bg-gradient-to-b from-slate-300/70 to-slate-300/40 bg-clip-text text-sm text-transparent",
+              "bg-gradient-to-b from-slate-300/70 to-slate-300/60 bg-clip-text text-sm text-transparent",
               GeistSans.className,
             )}
           >
-            <b>TL;DR</b>: Experienced in everything React - Typescript and
-            experienced in building apps from scratch (no designs) and
-            implementing all aspects of a software: from an internal UI library
-            to a secure API & unconventional db designs
+            <b>TL;DR</b>: Self-taught Software Developer with a strong focus on
+            building high-performance, responsive front-end applications using
+            NextJS and React. Experienced in API integration, performance
+            optimization, and creating intuitive user interfaces that meet
+            modern web standards. Proven track record in architecting scalable
+            solutions and collaborating closely with design teams to deliver
+            seamless user experiences
             <br />
             <br />
-            <b>I&apos;m Yash</b> - I&apos;m from India; having already 1 year of
-            working experience in a number of very small US startups in their
-            0-1 phase that made me a Founding Engineer in this span of time.
+            <b>I&apos;m Yash</b> - having worked professionally since 2023 in a
+            number of very small US startups in their 0-1 phase that made me a
+            Founding Engineer in this span of time.
           </p>
           <Link
             target="_blank"
